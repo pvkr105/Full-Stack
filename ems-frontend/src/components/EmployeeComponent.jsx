2f14e130c59614ce87532423990d860384c91a66
Vamshi Krishna Reddy <pvkr105@gmail.com>
2025-02-08 15:37:54 -0500

    add employee creation form with validation and navigation

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import { createEmployee } from "../services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 const EmployeeComponent = () => {
-  return <div>Employee Component</div>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const navigator = useNavigate();
+
+  const saveEmployee = (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      const employee = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      };
+      console.log(JSON.stringify(employee));
+
+      createEmployee(employee).then((res) => {
+        console.log(res.data);
+        navigator("/employees");
+      });
+    }
+  };
+
+  function validateForm() {
+    let valid = true;
+
+    const errorsCopy = { ...errors };
+
+    if (firstName.trim()) {
+      errorsCopy.firstName = "";
+    } else {
+      errorsCopy.firstName = "First Name is required";
+      valid = false;
+    }
+
+    if (lastName.trim()) {
+      errorsCopy.lastName = "";
+    } else {
+      errorsCopy.lastName = "Last Name is required";
+      valid = false;
+    }
+
+    if (email.trim()) {
+      errorsCopy.email = "";
+    } else {
+      errorsCopy.email = "Email is required";
+      valid = false;
+    }
+
+    setErrors(errorsCopy);
+
+    return valid;
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card col-md-6 offset-md-3 offset-md-3">
+          <h3 className="text-center">Add Employee</h3>
+          <div className="card-body">
+            <form>
+              <div className="form-group mb-2">
+                <label className="form-label">First Name:</label>
+                <input
+                  placeholder="First Name"
+                  name="firstName"
+                  className={`form-control ${
+                    errors.firstName ? "is-invalid" : ""
+                  }`}
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                {errors.firstName && (
+                  <div className="invalid-feedback">{errors.firstName}</div>
+                )}
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Last Name:</label>
+                <input
+                  placeholder="Last Name"
+                  name="lastName"
+                  className={`form-control ${
+                    errors.lastName ? "is-invalid" : ""
+                  }`}
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                {errors.lastName && (
+                  <div className="invalid-feedback">{errors.lastName}</div>
+                )}
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Email Id:</label>
+                <input
+                  placeholder="Email Address"
+                  name="email"
+                  className={`form-control ${errors.email ? "is-invalid" : ""}`}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors.email && (
+                  <div className="invalid-feedback">{errors.email}</div>
+                )}
+              </div>
+              <button className="btn btn-success" onClick={saveEmployee}>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeComponent;
